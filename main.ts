1cfcca168f4ca92098afacf85a15c9fc70e5cdf7
Markus Moser <m.moser@reply.de>
2024-11-11 15:18:41 +0100

    Prep for v1.0

--- main.ts
+++ main.ts
@@ -1,134 +1,102 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+/*
+  Privacy Glasses plugin for Obsidian
+  Copyright 2021 Jill Alberts
+  Licensed under the MIT License (http://opensource.org/licenses/MIT)
+*/
 
-// Remember to rename these classes and interfaces!
+import {
+    addIcon,
+    Plugin,
+    setIcon,
+} from "obsidian";
 
-interface MyPluginSettings {
-	mySetting: string;
+enum Level {
+    HidePrivate = "hide-private",
+    RevealAll = "reveal-all",
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+enum CssClass {
+    RevealAll = "private-mode-reveal-all",
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class PrivacyGlassesPlugin extends Plugin {
+    statusBar: HTMLElement;
+    statusBarSpan: HTMLSpanElement;
+    currentLevel: Level = Level.HidePrivate;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        this.statusBar = this.addStatusBarItem();
+        this.statusBar.addClass("mod-clickable")
+        this.statusBar.ariaLabel = "Toggle Private Mode"
+        this.statusBar.setAttr("data-tooltip-position", "top")
+        this.statusBar.onClickEvent(() => {
+            this.currentLevel = this.currentLevel == Level.HidePrivate ? Level.RevealAll : Level.HidePrivate;
+            this.updateGlobalRevealStyle();
+        });
+        this.statusBarSpan = this.statusBar.createSpan( { text: "" });
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        addIcon("eye", eyeIcon);
+        addIcon("eye-closed", eyeClosedIcon);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        this.addRibbonIcon("eye-closed", "Hide Private", () => {
+            this.currentLevel = Level.HidePrivate;
+            this.updateGlobalRevealStyle();
+        });
+        this.addRibbonIcon("eye", "Reveal all", () => {
+            this.currentLevel = Level.RevealAll;
+            this.updateGlobalRevealStyle();
+        });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        this.addCommand({
+            id: "privacy-glasses-hide-private",
+            name: "Hide Private",
+            callback: () => {
+                this.currentLevel = Level.HidePrivate;
+                this.updateGlobalRevealStyle();
+            },
+        });
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        this.addCommand({
+            id: "privacy-glasses-reveal-all",
+            name: "Reveal all",
+            callback: () => {
+                this.currentLevel = Level.RevealAll;
+                this.updateGlobalRevealStyle();
+            },
+        });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        this.app.workspace.onLayoutReady(() => {
+            this.updateGlobalRevealStyle();
+        });
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    updateGlobalRevealStyle() {
+        this.removeAllClasses();
+        this.setClassToDocumentBody(this.currentLevel);
+    }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    removeAllClasses() {
+        document.body.removeClass(
+            CssClass.RevealAll,
+        );
+    }
 
-	onunload() {
+    setClassToDocumentBody(currentLevel: Level) {
+        switch (currentLevel) {
+            case Level.HidePrivate:
+                setIcon(this.statusBarSpan, "eye-closed")
+                break;
+            case Level.RevealAll:
+                document.body.classList.add(CssClass.RevealAll);
+                setIcon(this.statusBarSpan, "eye")
+                break;
+        }
+    }
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+// https://icon-sets.iconify.design/ph/eye-closed-bold/
+const eyeClosedIcon = `<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 256"><path fill="currentColor" d="M234.4 160.8a12 12 0 0 1-10.4 18a11.8 11.8 0 0 1-10.4-6l-16.3-28.2a126 126 0 0 1-29.4 13.5l5.2 29.4a11.9 11.9 0 0 1-9.7 13.9l-2.1.2a12 12 0 0 1-11.8-9.9l-5.1-28.7a123.5 123.5 0 0 1-16.4 1a146.3 146.3 0 0 1-16.5-1l-5.1 28.7a12 12 0 0 1-11.8 9.9l-2.1-.2a11.9 11.9 0 0 1-9.7-13.9l5.2-29.4a125.3 125.3 0 0 1-29.3-13.5L42.3 173a12.1 12.1 0 0 1-10.4 6a11.7 11.7 0 0 1-6-1.6a12 12 0 0 1-4.4-16.4l17.9-31a142.4 142.4 0 0 1-16.7-17.6a12 12 0 1 1 18.6-15.1C57.1 116.8 84.9 140 128 140s70.9-23.2 86.7-42.7a12 12 0 1 1 18.6 15.1a150.3 150.3 0 0 1-16.7 17.7Z"/></svg>`;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+// https://icon-sets.iconify.design/ph/eye/
+const eyeIcon = `<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 256"><path fill="currentColor" d="M247.3 124.8c-.3-.8-8.8-19.6-27.6-38.5C194.6 61.3 162.9 48 128 48S61.4 61.3 36.3 86.3C17.5 105.2 9 124 8.7 124.8a7.9 7.9 0 0 0 0 6.4c.3.8 8.8 19.6 27.6 38.5c25.1 25 56.8 38.3 91.7 38.3s66.6-13.3 91.7-38.3c18.8-18.9 27.3-37.7 27.6-38.5a7.9 7.9 0 0 0 0-6.4ZM128 192c-30.8 0-57.7-11.2-79.9-33.3A130.3 130.3 0 0 1 25 128a130.3 130.3 0 0 1 23.1-30.8C70.3 75.2 97.2 64 128 64s57.7 11.2 79.9 33.2A130.3 130.3 0 0 1 231 128c-7.2 13.5-38.6 64-103 64Zm0-112a48 48 0 1 0 48 48a48 48 0 0 0-48-48Zm0 80a32 32 0 1 1 32-32a32.1 32.1 0 0 1-32 32Z"/></svg>`;
