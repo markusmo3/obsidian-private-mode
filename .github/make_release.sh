f7a8b3860877b9840af47484cc273da9906c0090
Markus Moser <m.moser@reply.de>
2025-01-16 14:11:18 +0100

    Add ability to make release locally

---
+++ .github/make_release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+PLUGIN_NAME=${1:-${PWD##*/}}
+
+rm -r ${PLUGIN_NAME}
+mkdir ${PLUGIN_NAME}
+for f in main.js manifest.json styles.scss styles.css; do
+  if [[ -f $f ]]; then
+      cp $f "${PLUGIN_NAME}/"
+  fi
+done
+zip -r "${PLUGIN_NAME}".zip "$PLUGIN_NAME"
